bd9139c8b1abf81ab91cb280b834cf06cbafbbad
Phil <richter.orga@web.de>
2024-12-27 14:33:12 +0100

    Day two completed

--- AdventOfCode/DayTwo/DayTwo.cs
+++ AdventOfCode/DayTwo/DayTwo.cs
@@ -9,18 +9,62 @@ class DayTwo
 {
     public static void StartDayTwo()
     {
-        int result = 0;
+        int saveCount = 0;
         string[] lines = System.IO.File.ReadAllLines("..\\..\\..\\DayTwo\\Input.txt");
 
-        foreach(string line in lines)
+        foreach (string line in lines)
         {
-            List<int> numbers = new List<int>();
-            string[] sNumbers = line.Split(' ');
+            int[] numbers = Array.ConvertAll(line.Split(' '), int.Parse);
 
-            foreach(string number in sNumbers)
+            if (IsRowValid(numbers))
             {
-                numbers.Add(Convert.ToInt32(number));   
+                saveCount++;
+            }
+            else
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    var correctedNumbers = numbers.Where((_, index) => index != i).ToArray();
+
+                    if (IsRowValid(correctedNumbers))
+                    {
+                        saveCount++;
+                        break;
+                    }
+                }
             }
         }
+        Console.WriteLine($"Number of save records: {saveCount}");
     }
+
+    private static bool IsRowValid(int[] numbers)
+    {
+        return IsValidDifference(numbers) && (IsSorted(numbers, true) || IsSorted(numbers, false));
+    }
+
+    private static bool IsValidDifference(int[] row)
+    {
+        for (int i = 0; i < row.Length - 1; i++)
+        {
+            int difference = row[i] - row[i + 1];
+            if (row[i] == row[i + 1] || Math.Abs(difference) >= 4)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static bool IsSorted(int[] row, bool ascending)
+    {
+        for (int i = 0; i < row.Length - 1; i++)
+        {
+            if (ascending && row[i] > row[i + 1] || !ascending && row[i] < row[i + 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
